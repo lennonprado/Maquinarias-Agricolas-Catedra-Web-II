f198dda619f28966add56c762cd6604d20db32a1
Pradin <pradin@Pradins-MacBook-Air.local>
2016-10-06 20:10:33 -0300

    Actualizo el archivo .sql para que cree la base si no existe y haga un truncate table si existen las tablas

--- sql/maquinarias.sql
+++ sql/maquinarias.sql
@@ -1,4 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 07-10-2016 a las 01:05:43
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `maquinarias`
+--
+CREATE DATABASE IF NOT EXISTS `maquinarias` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `maquinarias`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `caracteristicas`
+--
 
 CREATE TABLE `caracteristicas` (
   `id_caracteristica` int(11) NOT NULL,
@@ -8,6 +36,11 @@ CREATE TABLE `caracteristicas` (
   `car_status` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Truncar tablas antes de insertar `caracteristicas`
+--
+
+TRUNCATE TABLE `caracteristicas`;
 -- --------------------------------------------------------
 
 --
@@ -21,6 +54,11 @@ CREATE TABLE `categorías` (
   `cat_status` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Truncar tablas antes de insertar `categorías`
+--
+
+TRUNCATE TABLE `categorías`;
 -- --------------------------------------------------------
 
 --
@@ -41,6 +79,11 @@ CREATE TABLE `productos` (
   `prod_status` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Truncar tablas antes de insertar `productos`
+--
+
+TRUNCATE TABLE `productos`;
 --
 -- Índices para tablas volcadas
 --
@@ -85,3 +128,6 @@ ALTER TABLE `categorías`
 --
 ALTER TABLE `productos`
   MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
