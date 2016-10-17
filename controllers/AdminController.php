26c956a7cf80eb865b22278665b121b70c2fa7fc
Pradin <pradin@Pradins-MacBook-Air.local>
2016-10-17 18:57:46 -0300

    Productos view modificado
    d
    
    :q
    _
    
    dsdfsd

---
+++ controllers/AdminController.php
@@ -0,0 +1,71 @@
+<?php
+require('templates/ProductosView.php');
+require('models/ProductosModel.php');
+
+class ProductosController
+{
+  private $vista;
+  private $modelo;
+
+  function __construct()
+  {
+    $this->modelo = new ProductosModel();
+    $this->vista = new ProductosView();
+  }
+
+  function iniciar(){
+    $productos = $this->modelo->getProductos();
+    $this->vista->mostrar($productos);
+  }
+
+  function getImagenesVerificadas($imagenes){
+    $imagenesVerificadas = array();
+    for ($i=0; $i < count($imagenes['size']); $i++) {
+      if($imagenes['size'][$i]>0 && $imagenes['type'][$i]=="image/jpeg"){
+          $imagen_aux = array();
+          $imagen_aux['tmp_name']=$imagenes['tmp_name'][$i];
+          $imagen_aux['name']=$imagenes['name'][$i];
+          $imagenesVerificadas[]=$imagen_aux;
+      }
+    }
+
+    return $imagenesVerificadas;
+  }
+
+  function guardar(){
+    $producto = $_POST['prod_nombre'];
+    if(isset($_FILES['imagenes'])){
+      $imagenesVerificadas = $this->getImagenesVerificadas($_FILES['imagenes']);
+      if(count($imagenesVerificadas)>0){
+        if(!$this->filtro($tarea)){
+          $this->modelo->crearTarea($tarea,$imagenesVerificadas);
+          $this->vista->mostrarMensaje("La tarea se creo con imagen y todo!", "success");
+        }
+      }
+      else{
+        $this->vista->mostrarMensaje("Error con las imagenes", "danger");
+      }
+    }
+    else{
+        $this->vista->mostrarMensaje("La imagen es requerida","danger");
+    }
+
+    $this->iniciar();
+  }
+
+  function eliminar(){
+    $key = $_GET['id_producto'];
+    $this->modelo->eliminarProducto($key);
+    $productos = $this->modelo->getProductos();
+    $this->vista->getLista($productos);
+  }
+
+
+  function filtro($tarea){
+    return preg_match('/podria/',$tarea);
+  }
+
+
+}
+
+ ?>
